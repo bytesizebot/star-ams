8b9fca96f4b8ad10f3930242621fa0fcf8d65425
Ukechi Dikio <ukechi.dikio@entelect.co.za>
2026-02-17 11:49:34 +0200

    Test controller and java setup

---
+++ Backend/AnchorAPI/src/main/java/com/star/anchorapi/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.star.anchorapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Athlete Management API is running 🚀";
+    }
+}
---
+++ Backend/AnchorAPI/src/test/java/com/star/anchorapi/AnchorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.anchorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
